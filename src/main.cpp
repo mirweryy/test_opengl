2ff074a29d84395c69a72926c60481c977ca479b
Mirwery <wert456oxyy@gmail.com>
2025-05-03 15:05:49 +0300

    Add lib glad

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main(void)
 {
@@ -19,11 +21,17 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    if (!gladLoadGL())
+        return -1;
+
+    std::cout << "Versions: " << GLVersion.major << " " << GLVersion.minor;
+    glClearColor(1, 0, 0, 1);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
-        //glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
